4ac9e75af37eb10dd8adb66853387fa2d417eaf7
Moses Mushabe <mushdemosh143@gmail.com>
2025-05-21 12:40:36 +0300

    Initial recess class-work

---
+++ loopsandif.py
@@ -0,0 +1,33 @@
+import random
+# print("hello world")
+
+# print("Enter your age")
+
+# age = int(input())
+
+# print("So your age is "+ str(age), end = "?\n")
+
+# if age >= 18:
+#     print("You are an adult")
+# elif age >= 13:
+#     print("You are a tenager")
+# else:
+#     print("you are foolish")    
+    
+# cars = ["Volvo", "Benz"]
+
+# for car in cars:
+#     print(car)
+
+# 
+
+balance = 2000
+
+withdraw = int(input("Enter the amount you wanna withdraw"))
+
+if (withdraw == balance):
+    print("How about withdraw charges!")
+elif (withdraw > balance):
+    print("Your account balance is too low")
+else:
+    print("Successful!!!")    
